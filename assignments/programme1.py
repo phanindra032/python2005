239eb30b6893fa977fe28f507b46dd266c3ccfa5
phani <phanindra032@gmail.com>
2019-06-10 17:55:35 -0500

    I have added 1st question in AS1

---
+++ assignments/programme1.py
@@ -0,0 +1,18 @@
+
+print('Hello My name is  "Phani". I love Coding')
+print("This is 'my first program'.")
+
+X = int(input("please enter X value"))
+Y = int(input("please enter Y value"))
+Z = int(input("please enter Z value"))
+
+#Value1=X
+#print (Value1)
+print (X)
+#print('type(X)')
+print (Y)
+print (Z)
+
+type(X)
+type(Y)
+type(Z)
