68ec2ed8a125a7d067eb4eb7f65443ea352e2b43
phani <phanindra032@gmail.com>
2019-06-10 18:00:45 -0500

    i have added Q3 in AS1

---
+++ assignments/AS1Q3.py
@@ -0,0 +1,42 @@
+
+x= 10+20*(45+67.0)
+print("x= 10+20*(45+67.0)")
+print(x)
+
+
+print("******")
+
+
+
+
+a=20
+b=10
+x= ((a>b) and (b<a)) or (b<a)
+print("x= (True AND False) OR False")
+print(x)
+
+
+print("******")
+
+
+X=(((a>b) or (a>b)) and ((a>b) and (b<a)))
+print("x= (TRUE OR TRUE) AND (NOT False AND True)")
+print(x)
+
+print("******")
+
+
+e=3>89
+f=34>32
+x= (e or f)
+
+print("x= (3>89) or (34>32)")
+print(x)
+
+print("******")
+
+
+x= (e) and (e)
+print("x= (not true) and (false)")
+x=not True and false
+print(x)
