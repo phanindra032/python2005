9c0355ee7b131cb5c7d5a5d796e7de093b7d1a0e
phani <phanindra032@gmail.com>
2019-06-11 16:44:01 -0500

    have added greater number among 3 in greaternum.py

---
+++ assignments/greaternum.py
@@ -0,0 +1,14 @@
+m1= int(input("enter number m1 = "))
+m2= int(input("enter number m2 = "))
+m3= int(input("enter number m3 = "))
+
+if((m1>m2) and (m1>m3)):
+    print("m1 is greater number")
+#if(m1>m3):
+#    print("m1 is greater number")
+else:
+    if(m2>m3):
+        print("m2 is greater number")
+
+if(m2<m3):
+    print("m3 is greater number")
